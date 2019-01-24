d92f5d2445fa1bd67f61c325ddf2d349d3af91e1
si7f <46619100+si7f@users.noreply.github.com>
2019-01-25 00:20:10 +0700

    Add files via upload

---
+++ about_tampil.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/logoSIMPANG.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI ABSEN GURU <br> YAPIM SIMPANG KAWAT</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi absen guru di yapim simpang kawat merupakan sebuah sistem informasi yang di rancang untuk membantu sekolah semakin disipilin .
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Developer Kampoeng |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Smk Yapim</a>
+            <a href="#">Simpang Kawat</a>
+        </div>
+    </div>
+</div>
